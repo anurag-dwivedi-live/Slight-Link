1f97ece70df7874731a44ac03e0b368134de68c6
Anurag Dwivedi <111637089+anurag-dwivedi-live@users.noreply.github.com>
2024-07-21 17:39:32 +0530

    Added SQL File
    The most important database file to easy install database on any server like Apache or mySQL etc

---
+++ slightlink.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 21, 2024 at 02:07 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `slightlink`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `urls`
+--
+
+CREATE TABLE `urls` (
+  `id` int(4) NOT NULL,
+  `initial_url` text DEFAULT NULL,
+  `final_url` text DEFAULT NULL,
+  `clicks` int(4) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `urls`
+--
+
+INSERT INTO `urls` (`id`, `initial_url`, `final_url`, `clicks`, `created_at`) VALUES
+(31, 'https://chatgpt.com/', '68f5e', 1, '2024-07-21 16:16:12'),
+(32, 'https://github.com/anurag-dwivedi-live', '7e36a', 2, '2024-07-21 16:33:16');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `urls`
+--
+ALTER TABLE `urls`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `urls`
+--
+ALTER TABLE `urls`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
